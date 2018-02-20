099816f23550d2ad0c56ded837b8862274bac7de
Ben Lau <xbenlau@gmail.com>
2018-02-21 00:54:37 +0800

    Unit test: Install xbacktrace to capture backtrace log

--- tests/quickfutureunittests/main.cpp
+++ tests/quickfutureunittests/main.cpp
@@ -1,27 +1,12 @@
 #include <QString>
 #include <QtTest>
-#include <execinfo.h>
-#include <signal.h>
-#include <unistd.h>
 #include <TestRunner>
 #include <QFuture>
 #include <QSize>
 #include <QuickFuture>
+#include <XBacktrace.h>
 #include <QtQuickTest/quicktest.h>
 #include "quickfutureunittests.h"
-
-void handleBacktrace(int sig) {
-    void *array[100];
-    size_t size;
-
-    // get void*'s for all entries on the stack
-    size = backtrace(array, 100);
-
-    // print out all the frames to stderr
-    fprintf(stderr, "Error: signal %d:\n", sig);
-    backtrace_symbols_fd(array, size, STDERR_FILENO);
-    exit(1);
-}
 
 Q_DECLARE_METATYPE(QFuture<QSize>)
 
@@ -31,7 +16,7 @@ namespace AutoTestRegister {
 
 int main(int argc, char *argv[])
 {
-    signal(SIGSEGV, handleBacktrace);
+    XBacktrace::enableBacktraceLogOnUnhandledException();
 
     QGuiApplication app(argc, argv);
 
